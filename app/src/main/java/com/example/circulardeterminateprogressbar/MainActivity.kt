34ad8a3f59b340a65a8bcc79d8ce105db8e34448
Babitababy <kumaribabita2402@gmail.com>
2021-04-06 23:45:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/circulardeterminateprogressbar/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.circulardeterminateprogressbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
